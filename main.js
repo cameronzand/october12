ede15b5537de4e0347a3f635dfdda2e09c5cdd99
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-12 18:14:37 -0400

    progress

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+// var acc = document.getElementsByClassName("accordion");
+// var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    }
-}
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].onclick = function(){
+//         this.classList.toggle("active");
+//         this.nextElementSibling.classList.toggle("show");
+//     }
+// }
+
+
+
+//  $('.active').removeClass('panel');
+// $(this).addClass('panel');
+
+$('button').on('click',function(){
+  $('button').removeClass('active');
+  $(this).addClass('active');
+});
