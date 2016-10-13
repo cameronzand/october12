9fdafe34b9e973803b4b9dd99612fa8c28c34f20
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-13 16:03:45 -0400

    finished product

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## October 12th
+
+* Did fine with the html and css
+* Had problems with the jquery but got it resolved 
--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//     acc[i].onclick = function(){
-//         this.classList.toggle("active");
-//         this.nextElementSibling.classList.toggle("show");
-//     }
-// }
 
 
+var prevTarget = null;
+var prevPanel = null;
 
-//  $('.active').removeClass('panel');
-// $(this).addClass('panel');
-
-$('button').on('click',function(){
-  $('button').removeClass('active');
-  $(this).addClass('active');
+$('button').on('click', function(event){
+	var target = $(event.target);
+	var curTarget = target.attr("id").slice(-1);
+	var curPanel = $(target[0].nextElementSibling);
+  	if (prevTarget!==null && prevTarget!==curTarget){
+  		//Different already open
+  		prevPanel.toggleClass('active');
+  		prevTarget = curTarget;
+  	} else if (prevTarget === curTarget){
+  		//Same already open
+  		prevTarget = null;
+  	} else if (prevTarget === null){
+  		//None open
+  		prevTarget = curTarget;
+  	}
+  	prevPanel = curPanel;
+  	curPanel.toggleClass('active');
 });
+
+
